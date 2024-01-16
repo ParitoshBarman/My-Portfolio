5792ec471db1c0d948f6ca11274958e0829e9971
Paritosh Barman <barmanpari163@gmail.com>
2024-01-16 20:00:42 +0530

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-let typed = new Typed("#text", { strings: ["Frontend Developer", "Founder", "Web Developer", "Python Developer"], typeSpeed: 80, backSpeed: 50, backDelay: 1000, loop: true })
+let typed = new Typed("#text", { strings: ["Frontend Developer", "Full Stack Developer", "Founder", "Python Developer", "Web Developer"], typeSpeed: 80, backSpeed: 50, backDelay: 1000, loop: true })
+
+
+let navEle = document.querySelector(".navbar")
+let hidden = true;
+function myFunction(x) {
+    x.classList.toggle("change");
+    if(hidden==true){
+        navEle.style.display = "flex";
+        hidden = false
+    }else{
+        navEle.style.display = "none";
+        hidden = true;
+    }
+  }
